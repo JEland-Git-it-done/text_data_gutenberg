12528d7dc6891d54a453576fd8242f142514061b
jedel <jedeland@live.co.uk>
2020-01-22 15:50:36 +0100

    Created file and included simple NLTK function

--- venv/analysing_gutenberg_data.py
+++ venv/analysing_gutenberg_data.py
@@ -1,3 +1,16 @@
 import nltk
 
-nltk.download_shell()
+#nltk.download_shell()
+nltk.download("gutenberg")
+
+guten = nltk.corpus.gutenberg
+print("Gutenberg files : ", guten.fileids())
+
+def analyse_book():
+    print("Priting information about the selected book")
+    print("Please input your preference, the options are as follows, please ensure you type the name of the book correctly:\n", guten.fileids)
+    user_in = input()
+    book = nltk.corpus.gutenberg.words(user_in)
+
+macbth = nltk.corpus.gutenberg.words('shakespeare-macbeth.txt')
+print(len(macbth))
